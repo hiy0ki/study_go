3756cdf1be0009fb5f87310adcea71927205c33e
hiroki iyoda <hiroki.iyoda@borders.jp>
2017-01-10 11:39:47 +0900

    変数チェック

---
+++ first.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main(){
+    var name string = "hogehoge"
+    fmt.Println(name)
+
+    var num int = 100
+    fmt.Println(num)
+
+
+    var is bool = true
+    fmt.Println(is)
+
+    // 型推論
+    a := "hoge"
+    fmt.Println(a)
+    // a := 123
+    // fmt.Println(a)
+
+
+    fmt.Printf("hello world\n")
+}
