af5cccf4f13466b0056b5d723cd3b8169ac2372a
hiroki iyoda <hiroki.iyoda@borders.jp>
2017-01-27 10:03:59 +0900

    map 1

--- tour_of_go/map.go
+++ tour_of_go/map.go
@@ -1,1 +1,21 @@
 // https://go-tour-jp.appspot.com/moretypes/19
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+// mapリテラルはstructリテラルに似ているがキーが必要
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+}
+// https://go-tour-jp.appspot.com/moretypes/20
