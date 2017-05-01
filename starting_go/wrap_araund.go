7ea12b96f222160fb6ce657327dce78774b1dd0e
hiyoki <maha.tree.21@gmail.com>
2017-05-01 23:01:01 +0900

    update wrap around

--- starting_go/wrap_araund.go
+++ starting_go/wrap_araund.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main () {
+	// 最大値の定数
 	fmt.Printf("unit 32 max value = %d\n", math.MaxUint32)
 	
 
 	b := byte(255)
 	fmt.Println(b)
-	fmt.Println(b + byte(255))
-	fmt.Println(b + 1)
+	fmt.Println(b + 1) // == 0
+	fmt.Println(b + byte(255)) // -1したのと同じ
 }
