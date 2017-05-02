cecb99bfed93a9acc8cfe1f9e326492213fb4a12
hiyoki <maha.tree.21@gmail.com>
2017-05-02 11:56:47 +0900

    about interface{}

---
+++ starting_go/type_interface.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	/*
+	 intarface{}型は、
+	 - すべての型と互換性を持つ(javaのobject型的な?)
+	 - 初期値としてnil(具体的な値を持っていない状態)をもつ(javaのnull的な?)
+	*/
+	var x interface{}
+	fmt.Printf("%v", x)
+
+	// 色々代入できる
+	x = 123
+	fmt.Printf("%v", x)
+
+	x = "hogehoge"
+	fmt.Printf("%v", x)
+
+	x = 3.14
+	fmt.Printf("%v", x)
+
+	// ただし演算はできない
+	var a,b interface{}
+	a,b = 1, 2
+	fmt.Println(a + b) // エラー
+}
