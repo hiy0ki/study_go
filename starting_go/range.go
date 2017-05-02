47e263ad31b7711320f110c99136a496d7b8c27d
hiyoki <maha.tree.21@gmail.com>
2017-05-02 19:31:53 +0900

    about range

---
+++ starting_go/range.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fruits := [3]string{"apple", "banana", "grape"}
+
+	for i, s := range fruits {
+		// i はindex
+		// s は要素
+		fmt.Printf("fruits[%d]=%s\n", i, s)
+	}
+
+	// rangeは複数の要素を保持する性質を備えるデータ型に使える
+	// 文字列型をrangeで使うとrune型で取り出す
+	strings := "あいうえお"
+	for i, s := range strings{
+		fmt.Printf("strings[%d]=%s %#U\n", i, s, s)
+	}
+}
