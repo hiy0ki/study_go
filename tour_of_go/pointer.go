7615adb4292184d47a15e0f6b6e175c5140b796a
hiyoki <maha.tree.21@gmail.com>
2017-08-30 19:48:40 +0900

    add pointer sample

--- tour_of_go/pointer.go
+++ tour_of_go/pointer.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	i, j := 42,2701
+	i, j := 42, 2701
 
-	p := &i // & はそのオペランドへのポインタを引き出す
+	p := &i         // & はそのオペランドへのポインタを引き出す
 	fmt.Println(*p) // * はポインタの指す先の変数をしめす
 	*p = 21
 	fmt.Println(i)
-	
+
 	p = &j
 	*p = *p / 37
 	fmt.Println(j)
+	fmt.Println(&p)
 }
